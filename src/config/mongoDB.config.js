ef57ddef31c09940bd04774bb2cd25583d8508b9
Emiliano <emilianociurcilo@gmail.com>
2025-01-18 07:58:27 -0300

    entrega 2 v2

--- src/config/mongoDB.config.js
+++ src/config/mongoDB.config.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectMongoDB = async () => {
     try {
-        mongoose.connect("mongodb+srv://ciurciloemiliano2003:emi1234@cluster0.zm8ni.mongodb.net/proyecto-backend");
+        mongoose.connect("");
         console.log("MongoDB connected");
     } catch (error) {
         console.log(`Error: ${error}`);
